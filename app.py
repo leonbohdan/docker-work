f04734bc9128e2a6945d9147ac665a3032bcf3e0
Bohdan Leonets <leonetsbogdan@gmail.com>
2026-09-01 20:02:42 +0300

    feat: inject database connection string via environment variable instead of hardcoding

--- app.py
+++ app.py
@@ -5,8 +5,9 @@ import os
 app = Flask(__name__)
 
 myEnvVar = os.environ.get("MY_ENV_VAR", "development")
+DB_CONNECTION = os.environ.get("DB_CONNECTION", "")
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://app_user:1234@db-mysql/app_db'
+app.config['SQLALCHEMY_DATABASE_URI'] = DB_CONNECTION
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
